f50544f612555c3748f18ac7ae533c690d3b0e05
TommDevv <162940383+TommDevv@users.noreply.github.com>
2024-10-01 01:31:04 -0500

    Primera versión del menú
    unicamente hice el trabajo de frontend, en breve añado funcionalidad, musica y transisciones

---
+++ Menu/Scripts/libro_1.gd
@@ -0,0 +1,38 @@
+extends TextureButton
+
+func opacidad(newop: float) -> void:
+	modulate.a = clamp(newop, 0.0,1.0)	
+
+
+func _is_pressed():
+	return button_pressed
+
+func brillo(factor: float) -> void:
+	modulate.r = clamp(modulate.r * factor, 0.0, 2.0)
+	modulate.g = clamp(modulate.g * factor, 0.0, 2.0)
+	modulate.b = clamp(modulate.b * factor, 0.0, 2.0)
+
+func restaurar_brillo():
+	modulate = Color(1.0, 1.0, 1.0, modulate.a)
+
+func controlop() -> void:
+	if(is_hovered()):
+		opacidad(0.7)
+	else:
+		opacidad(1.0)
+
+
+func controlbril() -> void:
+	if(_is_pressed()):
+		brillo(1.5)
+	else:
+		restaurar_brillo()
+
+func _ready() -> void:
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	controlbril()
+	controlop()

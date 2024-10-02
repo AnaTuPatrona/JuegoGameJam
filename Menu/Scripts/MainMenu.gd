d980291a826a1a029eedf072780e484b43333c10
TommDevv <162940383+TommDevv@users.noreply.github.com>
2024-10-01 23:17:55 -0500

    Add files via upload

--- Menu/Scripts/MainMenu.gd
+++ Menu/Scripts/MainMenu.gd
@@ -1,7 +1,12 @@
 extends Control
 
+@onready var bg_music = $"Bg Music"
+@onready var sound_selected = $Select
+@onready var sound_hover = $Hover
+
 func _ready() -> void:
-	pass 
+	bg_music.play()
+	Transicion.iniciar()
 
 
 func _process(delta: float) -> void:
@@ -13,8 +18,22 @@ func _on_libro_1_pressed() -> void:
 
 
 func _on_libro_3_pressed() -> void:
+	sound_selected.play()
 	Transicion.cambiar_escena("res://juegoGatoConBotas/scenes/GatoConBotas/Escenario.tscn")
 
 
 func _on_libro_2_pressed() -> void:
+	sound_selected.play()
 	Transicion.cambiar_escena("res://juegoRitmo_RinRin/escenas_script/escenary.tscn")
+
+
+func _on_libro_2_mouse_entered() -> void:
+	sound_hover.play()
+
+
+func _on_libro_3_mouse_entered() -> void:
+	sound_hover.play()
+
+
+func _on_libro_1_mouse_entered() -> void:
+	sound_hover.play()
--- Menu/Scripts/transicion.gd
+++ Menu/Scripts/transicion.gd
@@ -3,11 +3,17 @@ extends CanvasLayer
 @onready var anim = $AnimationPlayer
 
 func cambiar_escena(path: String) -> void:
-	layer = 1
+	layer = 5
 	anim.play("Transicion")
 	await(anim.animation_finished)
 	get_tree().change_scene_to_file(path)
 	anim.play("Transicion2")
+	await(anim.animation_finished)
+	layer = -1
+
+func iniciar():
+	layer = 5
+	anim.play("inicio")
 	await(anim.animation_finished)
 	layer = -1
 

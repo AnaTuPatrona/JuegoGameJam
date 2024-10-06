998b6732d1580359591b6a5b6b5e34e25629f4bb
AnaTuPatrona <111031121+AnaTuPatrona@users.noreply.github.com>
2024-10-05 22:03:30 -0500

    ortografia tomás

--- JuegoPeterPan/Scripts/Scripter.gd
+++ JuegoPeterPan/Scripts/Scripter.gd
@@ -22,11 +22,11 @@ func _on_info_1_timeout() -> void:
 	get_tree().paused = true
 	$RetPeter.show_sprite()
 	await(ChoiceScene.show_animation())
-	await(ChoiceScene.display_text("El capitan Garfio capturo a Wendy y los niños!!"))
+	await(ChoiceScene.display_text("¡El Capitán Garfio capturo a Wendy y los niños!"))
 	await get_tree().create_timer(5.0).timeout
-	await(ChoiceScene.display_text("Ayudame a rescatarlos usando el puntero de tu mouse!! yo te sigo!!"))
+	await(ChoiceScene.display_text("¡Ayúdame a rescatarlos usando el puntero de tu mouse! ¡Yo te sigo!"))
 	await get_tree().create_timer(5.0).timeout
-	await(ChoiceScene.display_text("Evita las balas de cañon del capitan Garfio o estaremos perdidos!!"))
+	await(ChoiceScene.display_text("¡Evita las balas de cañón del Capitán Garfio o estaremos perdidos!"))
 	await get_tree().create_timer(5.0).timeout
 	get_tree().paused = false
 	$RetPeter.hide_sprite()
@@ -38,16 +38,16 @@ func _on_phase_2_timeout() -> void:
 	get_tree().paused = true
 	$RetGarfio.show_sprite()
 	await(ChoiceScene.show_animation())
-	await(ChoiceScene.display_text("A esa velocidad nunca nos alcanzará Ja Ja Ja!!"))
+	await(ChoiceScene.display_text("A esa velocidad nunca nos alcanzará ¡Ja Ja Ja!!"))
 	await get_tree().create_timer(3.0).timeout
 	$RetGarfio.hide_sprite()
 	$RetPeter.show_sprite()
-	await(ChoiceScene.display_text("Tenemos que ir más rápido!!"))
+	await(ChoiceScene.display_text("¡Tenemos que ir más rápido!"))
 	await get_tree().create_timer(3.0).timeout
-	await(ChoiceScene.display_option("¿Aceleramos el ritmo?","Ir más rapido", "Mejor lento pero seguro"))
+	await(ChoiceScene.display_option("¿Aceleramos el ritmo?","Ir más rapido.", "Mejor lento pero seguro."))
 	
 func on_option1():
-	await(ChoiceScene.display_text("Aceleremos el ritmo!!"))
+	await(ChoiceScene.display_text("¡Aceleremos el ritmo!"))
 	await get_tree().create_timer(3.0).timeout
 	$RetPeter.hide_sprite()
 	get_tree().paused = false
@@ -56,7 +56,7 @@ func on_option1():
 	$goodending.start()
 
 func on_option2():
-	await(ChoiceScene.display_text("Vale, sigamos sin prisas!!"))
+	await(ChoiceScene.display_text("Vale, sigamos sin prisas."))
 	await get_tree().create_timer(3.0).timeout
 	$RetPeter.hide_sprite()
 	get_tree().paused = false
@@ -69,7 +69,7 @@ func _on_badending_timeout() -> void:
 	get_tree().paused = true
 	$RetGarfio.show_sprite()
 	await(ChoiceScene.show_animation())
-	await(ChoiceScene.display_text("Finalmente lo estamos perdiendo!"))
+	await(ChoiceScene.display_text("¡Finalmente lo estamos perdiendo!"))
 	await get_tree().create_timer(3.0).timeout
 	$RetGarfio.hide_sprite()
 	$RetPeter.show_sprite()
@@ -80,7 +80,7 @@ func _on_badending_timeout() -> void:
 	await(ChoiceScene.display_text("¡¡Chicos!!"))
 	await get_tree().create_timer(3.0).timeout
 	$RetPeter.hide_sprite()
-	await(ChoiceScene.display_text("El capitán Garfio ha escapado"))
+	await(ChoiceScene.display_text("El Capitán Garfio ha escapado."))
 	await get_tree().create_timer(3.0).timeout
 	get_tree().paused = false
 	await(ChoiceScene.hide_animation())
@@ -91,10 +91,10 @@ func _on_goodending_timeout() -> void:
 	get_tree().paused = true
 	$RetPeter.show_sprite()
 	await(ChoiceScene.show_animation())
-	await(ChoiceScene.display_text("¡¡Casi lo alcanzamos!! ¡¡Sigue así!!"))
+	await(ChoiceScene.display_text("¡Casi lo alcanzamos! ¡¡Sigue así!!"))
 	await get_tree().create_timer(3.0).timeout
 	$RetPeter.hide_sprite()
-	await(ChoiceScene.display_text("Has alcanzado el barco del capitán Garfio"))
+	await(ChoiceScene.display_text("Has alcanzado el barco del Capitán Garfio."))
 	await get_tree().create_timer(3.0).timeout
 	get_tree().paused = false
 	ChoiceScene.hide_animation()
